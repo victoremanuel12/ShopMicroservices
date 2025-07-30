4469df5a18c4830de2d4cf0c66a57dc0403e25c1
victoremanuel12 <93943734+victoremanuel12@users.noreply.github.com>
2025-07-29 22:20:03 -0300

    Implement discount management service with EF Core
    - Removed `greet.proto` and `GreeterService`, added `discount.proto` and `DiscountService` for managing discounts.
    - Updated `Discount.Grpc.csproj` to include Entity Framework Core and Mapster.
    - Modified `Dockerfile` for improved build process and user management.
    - Added `DiscountContext` for SQLite database management and initial data seeding.
    - Updated configuration files for new service setup and database connection.
    - Created migration files for initial database schema including `Coupons` table.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static void MigrateDatabase(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            context.Database.MigrateAsync();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,60 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger) : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(p => p.ProductName == request.ProductName);
+            if (coupon is null)
+                coupon = new Coupon
+                {
+                    ProductName = "No Discount",
+                    Description = "No Discount Description",
+                    Amount = 0
+                };
+            logger.LogInformation("Discount is retrieved for ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Coupon cannot be null"));
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully created for ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(p => p.Id == request.Coupon.Id);
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, "Coupon not found"));
+            request.Coupon.Adapt(coupon);
+            dbContext.Coupons.Update(coupon);
+            await dbContext.SaveChangesAsync();
+            logger.LogInformation("Discount is successfully updated for ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(p => p.Id == request.Coupon.Id);
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, "Coupon not found"));
+            dbContext.Coupons.Remove(coupon);
+            await dbContext.SaveChangesAsync();
+
+            return new DeleteDiscountResponse{ Success = true};
+        }
+    }
+}
